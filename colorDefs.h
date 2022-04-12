9a9badc2595dc93df517ad7d5304959e4fbae26e
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-04-12 18:14:50 +0530

    Update and rename colorCfg.h to colorDefs.h

--- colorDefs.h
+++ colorDefs.h
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
